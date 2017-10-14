d9c82f3181671915a785348b8ee6eccb9abd2c44
Christoph <shynixn@gmail.com>
2017-10-14 15:20:19 +0200

    Updated sponsored minecraft-heads

--- petblocks-bukkit-plugin/src/main/java/com/github/shynixn/petblocks/bukkit/logic/business/configuration/MinecraftHeadConfiguration.java
+++ petblocks-bukkit-plugin/src/main/java/com/github/shynixn/petblocks/bukkit/logic/business/configuration/MinecraftHeadConfiguration.java
@@ -70,7 +70,7 @@ public class MinecraftHeadConfiguration extends CostumeConfiguration {
         this.items.clear();
         try {
             final Cipher decipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-            decipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(Base64Coder.decode("8a8tMA/MSlwQgq7SXMV1Fg=="), "AES"), new IvParameterSpec("RandomInitVector".getBytes("UTF-8")));
+            decipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(Base64Coder.decode("sLUkZ71lYwpeGgUd9ywltQ=="), "AES"), new IvParameterSpec("RandomInitVector".getBytes("UTF-8")));
             try (BufferedReader reader = new BufferedReader(new InputStreamReader(new CipherInputStream(JavaPlugin.getPlugin(PetBlocksPlugin.class).getResource("minecraftheads.db"), decipher)))) {
                 String s;
                 final String splitter = Pattern.quote(",");
