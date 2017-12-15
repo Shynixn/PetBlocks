4f9f5b32fdd650aaa325f6b68076d7fbf1f7b8ec
Christoph B <christoph@email.com>
2017-12-15 16:38:14 +0100

    Cleaned up the pathfinders.

--- petblocks-bukkit-plugin/src/main/java/com/github/shynixn/petblocks/bukkit/nms/v1_12_R1/OwnerPathfinder.java
+++ petblocks-bukkit-plugin/src/main/java/com/github/shynixn/petblocks/bukkit/nms/v1_12_R1/OwnerPathfinder.java
@@ -10,6 +10,33 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
+/**
+ * Pathfinder for the PetBlock.
+ * <p>
+ * Version 1.1
+ * <p>
+ * MIT License
+ * <p>
+ * Copyright (c) 2017 by Shynixn
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 public final class OwnerPathfinder extends PathfinderGoal {
     private final EntityInsentient entity;
     private PathEntity path;
@@ -19,13 +46,24 @@ public final class OwnerPathfinder extends PathfinderGoal {
     private int counter2;
     private int counter;
 
-    public OwnerPathfinder(EntityInsentient entitycreature, PetBlock petBlock) {
+    /**
+     * Initializes a new petblock owner pathfinder.
+     *
+     * @param entityCreature creatureUsingPath
+     * @param petBlock       petblock
+     */
+    public OwnerPathfinder(EntityInsentient entityCreature, PetBlock petBlock) {
         super();
-        this.entity = entitycreature;
+        this.entity = entityCreature;
         this.player = (Player) petBlock.getPlayer();
         this.petBlock = petBlock;
     }
 
+    /**
+     * Calculates the navigation path and returns true if found.
+     *
+     * @return success
+     */
     @Override
     public boolean a() {
         if (this.player == null) {
@@ -58,11 +96,15 @@ public final class OwnerPathfinder extends PathfinderGoal {
         return this.path != null;
     }
 
+    /**
+     * Abstract navigation.
+     */
     @Override
     public void c() {
-        if (this.entity instanceof CustomRabbit)
+        if (this.entity instanceof CustomRabbit) {
             this.entity.getNavigation().a(this.path, 2.5D);
-        else
+        } else {
             this.entity.getNavigation().a(this.path, 1D);
+        }
     }
 }
