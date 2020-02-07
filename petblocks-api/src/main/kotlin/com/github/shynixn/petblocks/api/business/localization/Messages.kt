377adc09c1182319793e67940426cb0fb188e88e
Shynixn <shynixn@gmail.com>
2020-02-07 13:49:19 +0100

    #302 Refactored lang file partially.

---
+++ petblocks-api/src/main/kotlin/com/github/shynixn/petblocks/api/business/localization/Messages.kt
@@ -0,0 +1,132 @@
+@file:Suppress("MayBeConstant")
+
+package com.github.shynixn.petblocks.api.business.localization
+
+/**
+ * Created by Shynixn 2020.
+ * <p>
+ * Version 1.5
+ * <p>
+ * MIT License
+ * <p>
+ * Copyright (c) 2020 by Shynixn
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+object Messages {
+    /**
+     * Message prefix.
+     */
+    val prefix: String = "prefix"
+
+    /**
+     * Gui title.
+     */
+    val guiTitle: String = "title-gui"
+
+    /**
+     * No permission message.
+     */
+    val noPermissionMessage = "message-no-permission"
+
+    /**
+     * Call success message.
+     */
+    val callSuccessMessage = "message-call-pet-success"
+
+    /**
+     * Call error message.
+     */
+    val callErrorMessage = "message-call-pet-error"
+
+    /**
+     * Despawn message.
+     */
+    val despawnMessage = "message-despawn-pet"
+
+    /**
+     * Custom head suggest prefix.
+     */
+    val customHeadSuggestPrefix = "suggest-customhead-prefix"
+
+    /**
+     * Custom head suggest clickable.
+     */
+    val customHeadSuggestClickable = "suggest-customhead-clickable"
+
+    /**
+     * Custom head suggest suffix.
+     */
+    val customHeadSuggestSuffix = "suggest-customhead-suffix"
+
+    /**
+     * Custom head suggest hover.
+     */
+    val customHeadSuggestHover = "suggest-customhead-hover"
+
+    /**
+     * Custom head success message.
+     */
+    val customHeadSuccessMessage = "message-customhead-success"
+
+    /**
+     * Custom head success message.
+     */
+    val customHeadErrorMessage = "message-customhead-error"
+
+    /**
+     * Has permission selection yes.
+     */
+    val permissionTranslationYes = "permission-translation-yes"
+
+    /**
+     * Has permission selection no.
+     */
+    val permissionTranslationNo = "permission-translation-no"
+
+    /**
+     * Rename suggest prefix.
+     */
+    val renameSuggestPrefix = "suggest-rename-prefix"
+
+    /**
+     * Rename suggest clickable.
+     */
+    val renameSuggestClickable = "suggest-rename-clickable"
+
+    /**
+     * Rename suggest suffix.
+     */
+    val renameSuggestSuffix = "suggest-rename-suffix"
+
+    /**
+     * Rename suggest hover.
+     */
+    val renameSuggestHover = "suggest-rename-hover"
+
+    /**
+     * Rename success message.
+     */
+    val renameSuccessMessage = "message-rename-success"
+
+    /**
+     * Rename error message.
+     */
+    val renameErrorMessage = "message-rename-error"
+}
