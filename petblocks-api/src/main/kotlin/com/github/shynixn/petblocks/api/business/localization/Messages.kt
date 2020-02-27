45c920a74ad6eb683896d721c5f73ffc7c1755b5
Shynixn <shynixn@gmail.com>
2020-02-27 13:11:31 +0100

    #302 Fixed lang file and messages contract.

--- petblocks-api/src/main/kotlin/com/github/shynixn/petblocks/api/business/localization/Messages.kt
+++ petblocks-api/src/main/kotlin/com/github/shynixn/petblocks/api/business/localization/Messages.kt
@@ -2,6 +2,8 @@
 
 package com.github.shynixn.petblocks.api.business.localization
 
+import com.github.shynixn.petblocks.api.business.annotation.Key
+
 /**
  * Created by Shynixn 2020.
  * <p>
@@ -33,120 +35,140 @@ object Messages {
     /**
      * Message prefix.
      */
+    @Key("prefix")
     var prefix: String = "prefix"
         private set
 
     /**
      * Gui title.
      */
-    var guiTitle: String = "title-gui"
+    @Key("gui.title")
+    var guiTitle: String = "gui.title"
         private set
 
     /**
      * No permission message.
      */
-    var noPermissionMessage = "message-no-permission"
+    @Key("messages.permissionNo")
+    var noPermissionMessage = "messages.permissionNo"
         private set
 
     /**
      * Call success message.
      */
-    var callSuccessMessage = "message-call-pet-success"
+    @Key("messages.callPetSuccess")
+    var callSuccessMessage = "messages.callPetSuccess"
         private set
 
     /**
      * Call error message.
      */
-    var callErrorMessage = "message-call-pet-error"
+    @Key("messages.callPetError")
+    var callErrorMessage = "messages.callPetError"
         private set
 
     /**
      * Despawn message.
      */
-    var despawnMessage = "message-despawn-pet"
+    @Key("messages.despawnPet")
+    var despawnMessage = "messages.despawnPet"
         private set
 
     /**
      * Custom head suggest prefix.
      */
-    var customHeadSuggestPrefix = "suggest-customhead-prefix"
+    @Key("messages.customHeadPrefix")
+    var customHeadSuggestPrefix = "messages.customHeadPrefix"
         private set
 
     /**
      * Custom head suggest clickable.
      */
-    var customHeadSuggestClickable = "suggest-customhead-clickable"
+    @Key("messages.customHeadClickable")
+    var customHeadSuggestClickable = "messages.customHeadClickable"
         private set
 
     /**
      * Custom head suggest suffix.
      */
-    var customHeadSuggestSuffix = "suggest-customhead-suffix"
+    @Key("messages.customHeadSuffix")
+    var customHeadSuggestSuffix = "messages.customHeadSuffix"
         private set
 
     /**
      * Custom head suggest hover.
      */
-    var customHeadSuggestHover = "suggest-customhead-hover"
+    @Key("messages.customHeadHover")
+    var customHeadSuggestHover = "suggest-customHead-hover"
         private set
 
     /**
      * Custom head success message.
      */
-    var customHeadSuccessMessage = "message-customhead-success"
+    @Key("messages.customHeadSuccess")
+    var customHeadSuccessMessage = "messages.customHeadSuccess"
         private set
 
     /**
      * Custom head success message.
      */
-    var customHeadErrorMessage = "message-customhead-error"
+    @Key("messages.customHeadError")
+    var customHeadErrorMessage = "messages.customHeadError"
         private set
 
     /**
      * Has permission selection yes.
      */
+    @Key("messages.permissionYes")
     var permissionTranslationYes = "permission-translation-yes"
         private set
 
     /**
      * Has permission selection no.
      */
+    @Key("messages.permissionNo")
     var permissionTranslationNo = "permission-translation-no"
         private set
 
     /**
      * Rename suggest prefix.
      */
+    @Key("messages.renamePrefix")
     var renameSuggestPrefix = "suggest-rename-prefix"
         private set
 
     /**
      * Rename suggest clickable.
      */
+    @Key("messages.renameClickable")
     var renameSuggestClickable = "suggest-rename-clickable"
         private set
 
     /**
      * Rename suggest suffix.
      */
+    @Key("messages.renameSuffix")
     var renameSuggestSuffix = "suggest-rename-suffix"
         private set
 
     /**
      * Rename suggest hover.
      */
+    @Key("messages.renameHover")
     var renameSuggestHover = "suggest-rename-hover"
         private set
 
     /**
      * Rename success message.
      */
+    @Key("messages.changeNameSuccess")
     var renameSuccessMessage = "message-rename-success"
         private set
 
     /**
      * Rename error message.
      */
+    @Key("messages.changeNameError")
     var renameErrorMessage = "message-rename-error"
         private set
 }
