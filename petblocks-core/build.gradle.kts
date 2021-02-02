7412a8e185e066dee20e798e88b2baa1fc8de131
Shynixn <shynixn@gmail.com>
2021-02-02 18:50:19 +0100

    #353 Fixed build.

--- petblocks-core/build.gradle.kts
+++ petblocks-core/build.gradle.kts
@@ -10,7 +10,15 @@ dependencies {
     compileOnly("org.slf4j:slf4j-api:1.7.25")
     compileOnly("com.zaxxer:HikariCP:3.2.0")
 
-    testCompile("org.xerial:sqlite-jdbc:3.23.1")
+    testCompile("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.3.0")
+    testCompile("com.fasterxml.jackson.core:jackson-databind:2.2.3")
+    testCompile("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
+    testCompile("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.3.9")
+    testCompile("com.google.inject:guice:4.1.0")
     testCompile("org.yaml:snakeyaml:1.24")
+    testCompile("org.slf4j:slf4j-api:1.7.25")
+    testCompile("com.zaxxer:HikariCP:3.2.0")
+
+    testCompile("org.xerial:sqlite-jdbc:3.23.1")
     testCompile("ch.vorburger.mariaDB4j:mariaDB4j:2.2.3")
 }
