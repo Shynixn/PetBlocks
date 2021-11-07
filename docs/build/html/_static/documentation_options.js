75cabcbfd33d7d4c3b2024eae851b85ec20f1f2a
Shynixn CI Actions <shynixn@email.com>
2021-11-07 19:52:17 +0000

    Automatic CI Documentation.

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '8.28.1',
+    VERSION: '8.28.2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
