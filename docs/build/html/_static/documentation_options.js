bccf46a8084304c720b64b4d0dcda6942d8fa4bc
Sphinx-rdt-wiki-action Agent <sphinx-rdt-wiki-agent@email.com>
2022-07-29 14:12:57 +0000

    Automatic CI Documentation.

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '8.31.2',
+    VERSION: '8.32.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
